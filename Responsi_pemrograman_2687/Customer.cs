b2153191c613183a0ede761f418924b223531c63
muhem <erwinsyahrul9@gmail.com>
2020-07-07 10:57:30 +0700

    1

---
+++ Responsi_pemrograman_2687/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer
+{
+    public class Customer
+    {
+        // PERINTAH: lengkapi property class customer, sesuai petunjuk soal
+        public string kodeCustomer { get; set; }
+        public string namaCustomer { get; set; }
+        public double totalPiutang { get; set; }
+    }
+}
